c1e7795378e5ffd6311f1d779cad5b5f72c1b87a
Alutulu <arthur.mata24@gmail.com>
2024-11-04 14:46:08 +0100

    fix indent spaces

--- python/camera.py
+++ python/camera.py
@@ -45,13 +45,13 @@ def main():
         cv.destroyAllWindows()
 
     else:
-    	picam2 = Picamera2()
-	camera_config = picam2.create_preview_configuration()
-	picam2.configure(camera_config)
-	picam2.start_preview(Preview.QTGL)
-	picam2.start()
-	time.sleep(2)
-	picam2.capture_file("test.jpg")
+        picam2 = Picamera2()
+        camera_config = picam2.create_preview_configuration()
+        picam2.configure(camera_config)
+        picam2.start_preview(Preview.QTGL)
+        picam2.start()
+        time.sleep(2)
+        picam2.capture_file("test.jpg")
     
     	#OLD
         #capture image
