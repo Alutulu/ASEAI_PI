7019e56732d3516739cca9466104ae1e623028ed
Alutulu <arthur.mata24@gmail.com>
2024-11-05 15:48:33 +0100

    azertyui

--- src/main.c
+++ src/main.c
@@ -12,23 +12,25 @@ int main(int argc, char *argv[])
 {
     int velocity = atoi(argv[1]);
     int sens = atoi(argv[2]);
+    GPIOInit(24);
+    PWMInit(0);
 
     if (velocity == 0)
     {
         printf("Velocite a zero\n");
-        PWMInit(0);
+
         PWMSetDisable(0);
+        GPIOSetDir(24, 0);
+        GPIOWrite(24, 0);
     }
     else
     {
         int duty_cycle = (PERIOD_PWM / 100) * velocity;
         printf("duty cycle %d period %d\n", duty_cycle, PERIOD_PWM);
 
-        GPIOInit(24);
         GPIOSetDir(24, 0);
         GPIOWrite(24, sens);
 
-        PWMInit(0);
         PWMSetPeriod(0, PERIOD_PWM);
         PWMSetDutyCycle(0, duty_cycle);
         PWMSetEnable(0);
